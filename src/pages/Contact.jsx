ecaf9f88a082a1feff21c8b829ed2495c7d699d9
Bitsapascal <bitapascal033@gmail.com>
2023-06-01 03:11:01 +0100

    Navigation avec react js

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import '../dist/css/all.css'
-// import $ from 'jquery'
-// import { useState } from "react";
+import $ from 'jquery'
+import { useState } from "react";
 const Contact = () => {
+    const [name, setName] = useState("");
+    const [annee, setAnnee] = useState("");
+    const [result, setResult] = useState("");
+    
+    const handleChange = (e) => {
+        setName(e.target.value);
+    };
+    
+    const handleChange2 = (e) => {
+        setAnnee(e.target.value);
+    };
+    
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        const form = $(e.target);
+        $.ajax({
+            type: "POST",
+            url: form.attr("action"),
+            data: form.serialize(),
+            success(data) {
+                console.log(data);
+                setResult(data);
+            },
+        });
+    }
     return (
         <div>
             <Layout />
