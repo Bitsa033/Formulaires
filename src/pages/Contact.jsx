5d9b678621e3f1f9ef8b9a20de29d699cfa9bcdf
Bitsapascal <bitapascal033@gmail.com>
2023-06-02 07:27:17 +0100

    ajout des packages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import Layout from '../components/Layout';
 import '../dist/css/all.css'
 import $ from 'jquery'
+// import { useForm } from "react-hook-form";
 import { useState } from "react";
 const Contact = () => {
+    // const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    // const onSubmit = data => console.log(data);
+    // console.log(watch("example"));
+
     const [name, setName] = useState("");
-    const [annee, setAnnee] = useState("");
+    const [phone, setPhone] = useState("");
     const [result, setResult] = useState("");
     
     const handleChange = (e) => {
@@ -13,7 +18,7 @@ const Contact = () => {
     };
     
     const handleChange2 = (e) => {
-        setAnnee(e.target.value);
+        setPhone(e.target.value);
     };
     
     const handleSumbit = (e) => {
@@ -35,26 +40,40 @@ const Contact = () => {
             <div className="col-md-4 offset-3">
                 <h1>Contact</h1>
                 <br />
-                <div className="form-group">
-                    <input type="text" className='form-control' placeholder='Name' />
-                </div>
-                <br />
-                <div className="form-group">
-                    <input type="email" className='form-control' placeholder='Email' />
-                </div>
-                <br />
-                <div className="form-group">
-                    <input type="tel" className='form-control' placeholder='Phone' />
-                </div>
-                <br />
-                <div className="form-group">
-                    <textarea type="text" className='form-control' placeholder='Type your message'>
-                    </textarea>
-                </div>
-                <br />
-                <div className="form-group">
-                    <input type="submit" className='form-control btn btn-success' />
-                </div>
+                <form action="" method="post" onSubmit={handleSumbit}>
+
+                    <div className="form-group">
+                        <input type="text" className='form-control' placeholder='Name' />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input type="email" className='form-control' placeholder='Email' />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input type="tel" className='form-control' placeholder='Phone' />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <textarea type="text" className='form-control' placeholder='Type your message'>
+                        </textarea>
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input type="submit" className='form-control btn btn-success' />
+                    </div>
+                </form>
+                {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+                {/* register your input into the hook by invoking the "register" function */}
+                {/* <input defaultValue="test" {...register("example")} /> */}
+                
+                {/* include validation with required or other standard HTML validation rules */}
+                {/* <input {...register("exampleRequired", { required: true })} /> */}
+                {/* errors will return when field validation fails  */}
+                {/* {errors.exampleRequired && <span>This field is required</span>} */}
+                
+                {/* <input type="submit" /> */}
+                {/* </form> */}
 
             </div>
         </div>
